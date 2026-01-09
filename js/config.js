eb959c9bc1a4788ab8dd14daf7a8e40baba7a219
Vovuskamputer <vovuskamputer@gmail.com>
2026-01-09 18:40:37 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 // ⚠️ НИКОГДА не коммить настоящий URL в публичный репозиторий!
 // Этот файл нужно заменять локально при развёртывании.
 
-const BACKEND_URL = "https://script.google.com/macros/s/EXAMPLE/exec";
+const BACKEND_URL = "https://script.google.com/macros/s/AKfycbyurxLkJ8WbHiPLenkKgnpWdigW7PwqT04ZLRWlUndy-bzXYGqtluy_aU2UY0V0q9Ab/exec";
+
